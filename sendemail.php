cad55f4733178a0023805fb642a3dc42d76813d5
Minghao Xu <xianyuhuanyue@gmail.com>
2014-10-13 22:37:54 +0800

    All finished, has problem? PHP can not use?

--- sendemail.php
+++ sendemail.php
@@ -5,17 +5,17 @@
 		'message'=>'Email sent!'
 	);
 
-    $name = @trim(stripslashes($_POST['name'])); 
-    $email = @trim(stripslashes($_POST['email'])); 
-    $subject = @trim(stripslashes($_POST['subject'])); 
-    $message = @trim(stripslashes($_POST['message'])); 
+    $name = @trim(stripslashes($_POST['name']));
+    $email = @trim(stripslashes($_POST['email']));
+    $subject = @trim(stripslashes($_POST['subject']));
+    $message = @trim(stripslashes($_POST['message']));
 
     $email_from = $email;
-    $email_to = 'email@gmail.com';
+    $email_to = 'phoenixxmh@126.com';
 
     $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
 
     $success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
 
     echo json_encode($status);
-    die; 
+    die;
